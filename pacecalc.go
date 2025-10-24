5d537574db8e7eb720c355e8591dc8e3a092546e
szelmat <szelessm@gmail.com>
2025-10-25 01:18:47 +0200

    add pace display function and integration test

--- pacecalc.go
+++ pacecalc.go
@@ -8,10 +8,36 @@ import (
 
 type PaceCalculator struct{}
 
+type PaceDisplay struct {
+	minutes int32
+	seconds int32
+}
+
 // Calculate the average pace of the run (min/km) based on distance and time.
 func (p *PaceCalculator) CalculatePace(distanceKm float64, result time.Duration) float64 {
+	if distanceKm <= 0 {
+		return 0
+	}
+
 	timeInMins := result.Minutes()
 
 	// Return rounded to two decimals
 	return math.Round(timeInMins/distanceKm*100) / 100
+}
+
+// Get the minute and second components for the pace instead of a decimal minute value
+func (p *PaceCalculator) GetDisplayPaceParts(pace float64) PaceDisplay {
+	mins := math.Floor(pace)
+	secs := math.Round((pace - mins) * 60)
+
+	// handle rounding overflow
+	if secs == 60 {
+		mins = mins + 1
+		secs = 0
+	}
+
+	return PaceDisplay{
+		minutes: int32(mins),
+		seconds: int32(secs),
+	}
 }
--- pacecalc_test.go
+++ pacecalc_test.go
@@ -1,27 +1,61 @@
 package pacecalc
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
+type PaceTestCase struct {
+	name            string
+	distanceKm      float64
+	duration        time.Duration
+	expectedPace    float64
+	expectedDisplay *PaceDisplay
+}
+
+const eps = 0.01
+
 var pc = PaceCalculator{}
 
-// Test pace calculation for 50 minute 10K time
-func Test50min10kPaceCalc(t *testing.T) {
-	want := 5.0
-	got := pc.CalculatePace(10, time.Minute*50)
+// Test pace calculation on various inputs
+func TestCalculatePace(t *testing.T) {
+	tests := []PaceTestCase{
+		{"50 min 10K", 10, time.Minute * 50, 5.0, nil},
+		{"3 hour marathon", 42, time.Hour * 3, 4.29, nil},
+	}
 
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
+	for i := range tests {
+		test := tests[i]
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			actualPace := pc.CalculatePace(test.distanceKm, test.duration)
+			if math.Abs(actualPace-test.expectedPace) > eps {
+				t.Errorf("%s: got %f: want %f", test.name, actualPace, test.expectedPace)
+			}
+		})
 	}
 }
 
-func Test3HourMarathonPaceCalc(t *testing.T) {
-	want := 4.29
-	got := pc.CalculatePace(42, time.Hour*3)
+func TestPaceDisplays(t *testing.T) {
+	tests := []PaceTestCase{
+		{"2 hour HM", 21, time.Hour * 2, 5.72, &PaceDisplay{minutes: 5, seconds: 43}},
+		{"15 min 5K", 5, time.Minute * 15, 3, &PaceDisplay{minutes: 3, seconds: 0}},
+		{"1 min 400m", 0.4, time.Minute, 2.5, &PaceDisplay{minutes: 2, seconds: 30}},
+	}
 
-	if got != want {
-		t.Errorf("got %f want %f", got, want)
+	for i := range tests {
+		test := tests[i]
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			actualDisplay := pc.GetDisplayPaceParts(pc.CalculatePace(test.distanceKm, test.duration))
+
+			if actualDisplay != *test.expectedDisplay {
+				t.Errorf(
+					"%s display: got %02d:%02d, want %02d:%02d",
+					test.name, actualDisplay.minutes, actualDisplay.seconds,
+					test.expectedDisplay.minutes, test.expectedDisplay.seconds)
+			}
+		})
 	}
 }
